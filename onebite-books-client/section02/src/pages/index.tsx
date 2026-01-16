91f03452705998bf4f799cdade13218d6ae1718e
Kykim0818 <Kykim0818@users.noreply.github.com>
2026-01-16 21:13:52 +0900

    save recent client changes

---
+++ onebite-books-client/section02/src/pages/index.tsx
@@ -0,0 +1,49 @@
+// CSS Module
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode } from "react";
+import style from "./index.module.css";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
+
+export const getServerSideProps = async () => {
+
+  const allBooks = await fetchBooks();
+  const recoBooks = await fetchRandomBooks();
+
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(allBooks);
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {recoBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {allBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
