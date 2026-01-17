a972b79dc01919eb7dda21926a16659307f9314c
Kykim0818 <Kykim0818@users.noreply.github.com>
2026-01-17 10:49:11 +0900

    feat: seo 등록

--- onebite-books-client/section02/src/pages/index.tsx
+++ onebite-books-client/section02/src/pages/index.tsx
@@ -1,12 +1,12 @@
-// CSS Module
-import SearchableLayout from "@/components/searchable-layout";
-import { ReactNode } from "react";
-import style from "./index.module.css";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
+import SearchableLayout from "@/components/searchable-layout";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import { InferGetStaticPropsType } from "next";
+import { ReactNode } from "react";
+import style from "./index.module.css";
+import Head from "next/head";
+
 
 export const getStaticProps = async () => {
 
@@ -29,6 +29,13 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   console.log(allBooks);
   return (
+    <>
+    <Head>
+      <title>한입북스</title>
+      <meta property="og:image" content="/thumbnail.png" />
+      <meta property="og:title" content="한입북스" />
+      <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요" />
+    </Head>
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
@@ -43,6 +50,7 @@ export default function Home({
         ))}
       </section>
     </div>
+    </>
   );
 }
 
