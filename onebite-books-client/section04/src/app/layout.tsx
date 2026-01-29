61f5e51a0f0edc76dd23d3f4dd373cad91fd40ed
Kykim0818 <kykim@seegene.com>
2026-01-29 14:17:15 +0900

    feat: fetch 기능 적용

--- onebite-books-client/section04/src/app/layout.tsx
+++ onebite-books-client/section04/src/app/layout.tsx
@@ -1,6 +1,25 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+  );
+  if (!response.ok) {
+    return <footer>제작 @kykim</footer>;
+  }
+
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+  return (
+    <footer>
+      <div>제작 @kykim</div>
+      <div>총 {bookCount}권의 도서를 제공하는 사이트입니다. </div>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +34,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
