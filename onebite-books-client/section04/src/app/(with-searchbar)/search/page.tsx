492246d4948aab7229d82210fc0f5f3abd17f43a
Kykim0818 <kykim@seegene.com>
2026-01-30 17:31:24 +0900

    풀라우터 캐시 설정 추가

--- onebite-books-client/section04/src/app/(with-searchbar)/search/page.tsx
+++ onebite-books-client/section04/src/app/(with-searchbar)/search/page.tsx
@@ -9,6 +9,7 @@ export default async function Page({
   const { q } = await searchParams;
   const reponse = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q || ""}`,
+    { cache: "force-cache" },
   );
   if (!reponse.ok) {
     return <div>오류가 발생했습니다...</div>;
