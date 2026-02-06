0dce64710d4ede036b74bc2bf5c85e6e2221d934
Kykim0818 <kykim@seegene.com>
2026-02-06 14:29:46 +0900

    feat: 서버액션 revalidate 태그 추가, 및 서버액션 시 UI처리 추가

--- onebite-books-client/section04/src/actions/create-review.action.ts
+++ onebite-books-client/section04/src/actions/create-review.action.ts
@@ -1,12 +1,17 @@
 "use server";
 
-export async function createReviewAction(formData: FormData) {
+import { revalidateTag } from "next/cache";
+
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요",
+    };
   }
 
   try {
@@ -20,8 +25,19 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       },
     );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     console.log(response.status);
+    revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
+    return {
+      status: false,
+      error: `리뷰 작성에 실패했습니다 : ${err}`,
+    };
   }
 }
